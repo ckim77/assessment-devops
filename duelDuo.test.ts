eb160e21957707407052dc4e881a9394ad87dbfe
Chris Kim <ckjr92@gmail.com>
2021-11-19 11:52:44 -0700

    finsihed testing

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -16,5 +16,19 @@ afterAll(async () => {
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
+    await driver.sleep (2000)
     expect(displayed).toBe(true)
+})
+
+test("choices appear after clicking draw", async () => {
+    await driver.findElement(By.id("draw")).click()
+    await driver.sleep (2000)
+    const choices = await driver.findElement(By.id('choices'))
+    const shown = await choices.isDisplayed()
+    expect(shown).toBe(true)
+})
+
+test ("added bots appear after clicking add bot", async () => {
+    await driver.findElement(By.xpath("//a[@class = 'bot-btn']")).click()
+    await driver.sleep (2000)
 })
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,16 @@
 const {shuffleArray} = require('./utils')
 
+let arr = [1,2,3,4,5]
+
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test ("test to see that shuffleArray length is same as input length of input array", () => {
+        expect(shuffleArray(arr).length).toBe(5)
+    })
+
+    test ("test to see if array contents are in the new output array", () => {
+        expect(shuffleArray(arr)).toContain(1)
+    })
+
 })
+
